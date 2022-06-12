b50ae32f15ebcd9458caa4a683e7b23d374d83f0
Steven Hangger <longhronshen@hotmail.com>
2022-06-12 15:08:52 +0800

    chore(*): *

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+echo "Checking docker..."
+if [[ "$(which docker)" == "" ]]; then
+    echo "Docker not found. Installing."
+    curl -sSL https://get.daocloud.io/docker | sh
+    curl -L https://get.daocloud.io/docker/compose/releases/download/v2.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    echo "Docker installed."
+fi
+
+mkdir -p /etc/systemd/system/docker.service.d
+envsubst < ./http-proxy.conf.template > /etc/systemd/system/docker.service.d/http-proxy.conf
+
+systemctl daemon-reload
+systemctl show --property Environment docker
+
+systemctl restart docker
